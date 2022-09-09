22f176083e79d69cc8a2de90e9858429c81d1c8f
Poudlardo <adamtbatou@free.fr>
2022-09-09 09:23:35 +0200

    turn consolelog game into ui game

--- rock_paper_scissors/game.js
+++ rock_paper_scissors/game.js
@@ -4,21 +4,31 @@ function getComputerChoice() {
     return choice;
 }
 
+let score = 0;
+
 function singleRound(playerSelection, computerSelection) {
-if (playerSelection.toLowerCase() == "rock") {
+if (score == 5) {
+    return "congratulations! we have a winner here";
+} else if (playerSelection.toLowerCase() == "rock") {
     if (computerSelection == 'scissors') {
+        score += 1;
+        add.innerHTML = score;
         return "You Win! Rock beats scissors";
     } else {
         return "You Lose! Paper beats rock";
     }
 } else if (playerSelection.toLowerCase() == "paper") {
     if (computerSelection == 'rock') {
+        score += 1;
+        add.innerHTML = score;
         return "You Win! Paper beats rock";
     } else {
         return "You Lose! Scissors beats paper";
     }
 } else if (playerSelection.toLowerCase() == 'scissors') {
     if (computerSelection == 'paper') {
+        score += 1;
+        add.innerHTML = score;
         return "You Win! Scissors beats paper";
     } else {
         return "You Lose! Rock beats scissors";
@@ -28,10 +38,12 @@ if (playerSelection.toLowerCase() == "rock") {
 }    
 }
 
-function game() {
-    for (let i = 0; i < 5; i++) {
-        let playerSelection = prompt("Rock, Paper, or Scissors ?");
-        console.log(singleRound(playerSelection, getComputerChoice()));
-     }
-}
+let buttons = document.querySelectorAll('button');
 
+let source = document.getElementById('results');
+for (let i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener('click', (e)=> {
+	 let playerSelection = e.target.id ;
+	 source.innerHTML = "<br>" + "Results : " + singleRound(playerSelection, getComputerChoice());
+    }  );
+}

2d38b0974e977ac7877c56eced3c80410658f200
Poudlardo <adamtbatou@free.fr>
2022-09-27 14:47:28 +0200

    add first buttons + link them with js functions

--- Calculette/calculette.js
+++ Calculette/calculette.js
@@ -1,1 +1,42 @@
 
+// basic math operators
+function add(a,b) {
+ return a + b;
+
+}
+
+function substract(a,b) {
+    return a - b;
+}
+
+
+function multiply(a,b) {
+    return a * b;
+}
+
+
+function divide(a,b) {
+    return a / b;
+}
+
+// call one above function on the numbers
+function operate(operator,a,b) {
+    if (operator === "+") {
+        console.log(add(a,b));
+    } else if (operator === "-") {
+        console.log(substract(a,b));
+    } else if (operator === "*"){
+        console.log(multiply(a,b));
+    } else if (operator === "/") {
+        console.log(divide(a,b));
+    }
+}
+
+function afficherValeurs(valeur) {
+    let afficher = document.querySelector("#affichage");
+        afficher.textContent = `${valeur}`;
+}
+
+function toutEffacer() {
+
+}

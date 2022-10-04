3dce233ab94d82f9a9e00cd8b9da1bdd8bd8d0df
poudlardo <adamtbatou@free.fr>
2022-10-04 19:39:39 +0200

    mettre en marche la calculette

--- Calculette/calculette.js
+++ Calculette/calculette.js
@@ -1,42 +1,59 @@
-
 // basic math operators
 function add(a,b) {
- return a + b;
-
+    return document.getElementById('affichage').innerHTML = a + b;
 }
 
 function substract(a,b) {
-    return a - b;
+    return document.getElementById('affichage').innerHTML = a - b;
 }
-
 
 function multiply(a,b) {
-    return a * b;
+    return document.getElementById('affichage').innerHTML = a * b;
 }
-
 
 function divide(a,b) {
-    return a / b;
-}
-
-// call one above function on the numbers
-function operate(operator,a,b) {
-    if (operator === "+") {
-        console.log(add(a,b));
-    } else if (operator === "-") {
-        console.log(substract(a,b));
-    } else if (operator === "*"){
-        console.log(multiply(a,b));
-    } else if (operator === "/") {
-        console.log(divide(a,b));
-    }
-}
-
-function afficherValeurs(valeur) {
-    let afficher = document.querySelector("#affichage");
-        afficher.textContent = `${valeur}`;
+    return document.getElementById('affichage').innerHTML = a / b;
 }
 
 function toutEffacer() {
+      document.getElementById('affichage').innerHTML = "";
+}
 
+// 1) l'utilisateur clique sur les bouttons pour écrire son opération
+
+function afficherValeurs(valeur) {
+    if (valeur === undefined) {
+        document.getElementById('affichage').innerHTML = "";
+    } else {
+        return document.getElementById('affichage').innerHTML += `${valeur}`;
+    }
+}    
+
+// 3) calculer avec valeurs définies
+function operate() {
+    let calcul = document.getElementById('affichage').innerHTML;
+    let a = parseInt(calcul); 
+
+    let resteValeurs = String(calcul).replace(`${a}`, ""); 
+    let resteArr = [];
+
+for (let i=0; i < resteValeurs.length; i++) {
+        resteArr.push(resteValeurs[i])
+}
+
+let oper = resteArr[0];
+let b = parseInt(resteArr.splice(1).join(""));
+
+console.log(oper);
+console.log(b);
+
+if (oper === '+') {
+    add(a,b);
+} else if (oper === '-') {
+    substract(a,b);
+} else if (oper === '/') {
+    divide(a,b);
+ } else if (oper === '*') {
+    multiply(a,b);
+ }
 }

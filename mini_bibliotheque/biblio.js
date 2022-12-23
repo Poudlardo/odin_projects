08b556ffaa5c33a91ef5a8703f4e06767fed55f1
poudlardo <adamtbatou@free.fr>
2022-12-23 09:31:22 +0100

    make card creation and deletion work

--- mini_bibliotheque/biblio.js
+++ mini_bibliotheque/biblio.js
@@ -1,61 +1,133 @@
 let myLibrary = [{
-  title: 'The Hobbit',
+  Titre: 'The Hobbit',
   author: 'J. R. R. Tolkien',
   pages: '287',
-  isRead: 'Not Read yet'
+  isRead: 'Pas Lu'
 },
 {
-  title: 'Da Vinci Code',
+  Titre: 'Da Vinci Code',
   author: 'Dan Brown',
   pages: '571',
-  isRead: 'Read'
+  isRead: 'Lu'
 }];
 
-function Book(title, author, pages, isRead) {
-    this.title = title
-    this.author = author
-    this.pages = pages
-    this.isRead = isRead
-    this.info = function() {
-        console.log(title + " by " + author + ", " + pages + " pages, " + isRead);
-    }
+function Book(Titre, author, pages, isRead) {
+    this.Titre = Titre;
+    this.author = author;
+    this.pages = pages;
+    this.isRead = isRead;
 }
 
-//const theHobbit = new Book('The Hobbit','J. R. R. Tolkien','287', 'Not Read yet');
-//const daVinciCode = new Book('Da Vinci Code','Dan Brown','571','Read');
-//theHobbit.info() ;
-//daVinciCode.info();
+function pushInMyLibrary(Titre, author, pages, isRead) {
+  let aBook = new Book(Titre, author, pages, isRead);
+  myLibrary.push(aBook);
+  createCardItem();
+}
 
-function addBookToLibrary() {
-  const button = document.getElementById('submit');
+function createCardItem() {  
 
-  // take user's inputs
-  const title = document.getElementById("title");
-  const author = document.getElementById("author");
-  const isRead = document.getElementById("isRead");
-  const pages = document.getElementById("pages");
+let cardContainer = document.querySelector('.card-container');
 
-  // store new book objects in array button onclick 
-button.addEventListener('click', () => {
-     myLibrary.push({
-        title: title.value,
-        author: author.value,
-        pages: pages.value,
-        isRead: isRead.value
-     })
+const removeDivs = document.querySelectorAll(".card");
+for (let i = 0; i < removeDivs.length; i++) {
+  removeDivs[i].remove();
+}
+
+let index = 0;
+myLibrary.forEach( Books =>  {
+      let createCard = document.createElement('div');
+      createCard.setAttribute('class', 'card');
+      cardContainer.appendChild(createCard);
+
+
+      for (key in Books) {
+      let paragraph = document.createElement('p');
+      let checkboxes = document.createElement('input');
+      checkboxes.setAttribute('id', 'read')
+      paragraph.textContent = `${key}: ${Books[key]}`;
+      createCard.appendChild(paragraph);
+  };
+
+        // Create remove book button and add class attribute for each array card
+      const removeBookButton = document.createElement("button");
+      removeBookButton.classList.add("remove-book-button");
+      removeBookButton.textContent = "Enlever de la Bibliothèque"; 
+      removeBookButton.dataset.linkedArray = index;
+      createCard.appendChild(removeBookButton);
+      removeBookButton.addEventListener("click", removeBookFromLibrary);
+
+     // Create read status button and add class attribute for each array card
+      const readStatusButton = document.createElement("button");
+      readStatusButton.classList.add("read-status-button");
+      readStatusButton.textContent = "Lu / Pas Lu";
+
+     // Link the data attribute of the toggle read button to the array and card
+      readStatusButton.dataset.linkedArray = index;
+      createCard.appendChild(readStatusButton);
+
+     // Create event listener/toggle logic for array objects prototype for read status change
+      readStatusButton.addEventListener("click", toggleReadStatus);
+
+  function toggleReadStatus() {
+    let retrieveBookToToggle = readStatusButton.dataset.linkedArray;
+    Book.prototype = Object.create(Book.prototype);
+    const toggleBook = new Book();
+
+    // Run check to see what read value is present to toggle from
+    if (myLibrary[parseInt(retrieveBookToToggle)].isRead == "Lu") {
+      toggleBook.isRead = "Pas Lu";
+      myLibrary[parseInt(retrieveBookToToggle)].isRead = toggleBook.isRead;
+    } else if (myLibrary[parseInt(retrieveBookToToggle)].isRead == "Pas Lu") {
+      toggleBook.isRead = "Lu";
+      myLibrary[parseInt(retrieveBookToToggle)].isRead = toggleBook.isRead;
+    }
+    createCardItem();
+  }
+
+      function removeBookFromLibrary() {
+        let retrieveBookToRemove = removeBookButton.dataset.linkedArray;
+        myLibrary.splice(parseInt(retrieveBookToRemove), 1);
+        createCard.remove();
+        createCardItem();
+      }
+      index++;
+}) 
+}
+// variables
+
+
+
+// action to display the form, works
+let displayForm = document.getElementById('show-form');
+displayForm.addEventListener('click', ()=> {
+let form = document.getElementById('form-block');
+
+if (form.style.display === 'none') {
+
+  form.style.display = 'block';
+} else {
+
+  form.style.display = 'none';
+}
 })
 
+let submitData = document.getElementById('submitBtn');
+submitData.addEventListener('click', formDatas);
 
+function formDatas() {
+let titleValue = document.getElementById('Titre').value;
+let authorValue = document.getElementById('author').value;
+let pagesValue = document.getElementById('pages').value;
+let readOrNot = document.getElementById('isRead').value;
+
+if (titleValue == "" || authorValue == "" || pagesValue == "" || readOrNot =="") {
+  return;
 }
 
-myLibrary.forEach(displayObject) ;
+pushInMyLibrary(titleValue,authorValue,pagesValue,readOrNot);
 
-function displayObject() {
-    const cardContainer = document.querySelector('.card-container')
-  cardContainer.innerHTML += `<div class="card">${cardContainer.childElementCount}</div>`
+  // Reset the form after successful submission
+  document.getElementById("form").reset();
 }
 
-
-
-
-console.log(myLibrary);
+createCardItem();

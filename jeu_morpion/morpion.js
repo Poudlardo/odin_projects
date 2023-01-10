b4595e22ef80d7b5178137b1f6366f397a87cce9
poudlardo <adamtbatou@free.fr>
2023-01-10 23:25:17 +0100

    write first function

---
+++ jeu_morpion/morpion.js
@@ -0,0 +1,10 @@
+const plateau = {
+    plateauValeurs : ['X', 'X', 'O', '0', 'X', 'O', 'O', 'X', 'X'],
+
+    plateauRendu : function plateauRenduVisuel() {
+
+        for (i=0; i < plateauValeurs.length; i++) {
+
+        }
+    }
+}

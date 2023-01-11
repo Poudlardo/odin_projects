c6c64fd355ba2c76f9fa1bffa068e588b7a59939
poudlardo <adamtbatou@free.fr>
2023-01-11 23:09:48 +0100

    implement first functions and module

--- jeu_morpion/morpion.js
+++ jeu_morpion/morpion.js
@@ -1,10 +1,23 @@
-const plateau = {
-    plateauValeurs : ['X', 'X', 'O', '0', 'X', 'O', 'O', 'X', 'X'],
+let gameboardModule = (() => {
+    let gameboard = ['X', 'O','X','X','O','O','O','X','X'];
+    return {gameboard};
+})();
 
-    plateauRendu : function plateauRenduVisuel() {
+let displayControllerModule = (()=> {
+    // put logic here
+})();
 
-        for (i=0; i < plateauValeurs.length; i++) {
+let createPlayer = (playerName, playerNumber, assignedXO) => {
+    let getPlayerName = () => { playerName;
+    console.log('This is the name of player' + playerNumber + '....' + playerName); }
+    return {getPlayerName,playerName,playerNumber, assignedXO};
+};
 
-        }
+let Mohamed = createPlayer('Mohamed', 1, 'X');
+let Fabrice = createPlayer('Fabrice', 2, '0');
+
+(function renderArrayToGameBoard() {
+       for (i=0;i < allBoxes.length;i++) {
+       gameboardModule.gameboard[i] = document.querySelectorAll('.grid-box')[i].textContent;
     }
-}
+})()

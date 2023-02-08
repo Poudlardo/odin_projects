58cfb96845fbefe65487ae7054835829a7b86c25
Poudlardo <55178305+Poudlardo@users.noreply.github.com>
2023-02-08 11:55:54 +0100

    finish project

--- jeu_morpion/morpion.js
+++ jeu_morpion/morpion.js
@@ -1,113 +1,89 @@
-let gameboardModule = (() => {
-    let gameboard = [' ',' ',' ',
-                     ' ',' ',' ',
-                     ' ',' ',' '];
-    return {gameboard};
-})();
+const X_CLASS = 'x'
+const CIRCLE_CLASS = 'circle'
+const WINNING_COMBINATIONS = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6]
+]
+const cellElements = document.querySelectorAll('[data-cell]')
+const board = document.getElementById('board')
+const winningMessageElement = document.getElementById('winningMessage')
+const restartButton = document.getElementById('restartButton')
+const winningMessageTextElement = document.querySelector('[data-winning-message-text]')
+let circleTurn
 
-let firstPlayerFunc = (() => {
-    const joueurUnOuDeux = [0,1];
-    let joueurUodLength = joueurUnOuDeux.length;
-    let randomNumber = Math.floor(Math.random()*joueurUodLength);
-    let firstPlayer = joueurUnOuDeux[randomNumber];
-    return {firstPlayer};
-})()
+startGame()
 
-// action au lancement de la partie
-let submitData = document.getElementById('commencer');
-submitData.addEventListener('click', startGame);
+restartButton.addEventListener('click', startGame)
 
 function startGame() {
-
-const joueurUn = document.getElementById('joueurUn').value;
-const joueurDeux = document.getElementById('joueurDeux').value;
-
-const playerOne = createPlayer(joueurUn, 0, "X");
-const playerTwo = createPlayer(joueurDeux, 1, "O");
-
-const showform = document.getElementById('showform');
-showform.style.display = 'none';
-
-    if (joueurUn == "" || joueurDeux =="") {
-            return;
-        }
-
-    if (firstPlayerFunc.firstPlayer == playerOne.playerNumber) {
-        const formarea = document.getElementById('formarea');
-        formarea.textContent = `${joueurUn} tu commences !`;
-        return firstPlayerFunc.firstPlayer;
-    } else if (firstPlayerFunc.firstPlayer == playerTwo.playerNumber) {
-        const formarea = document.getElementById('formarea');
-        formarea.textContent = `${joueurDeux} tu commences !`;
-        return firstPlayerFunc.firstPlayer
-    }
+  circleTurn = false
+  cellElements.forEach(cell => {
+    cell.classList.remove(X_CLASS)
+    cell.classList.remove(CIRCLE_CLASS)
+    cell.removeEventListener('click', handleClick)
+    cell.addEventListener('click', handleClick, { once: true })
+  })
+  setBoardHoverClass()
+  winningMessageElement.classList.remove('show')
 }
 
-let createPlayer = (playerName, playerNumber, assignedXO) => {
-    let getPlayerName = () => { playerName;
-    console.log('This is the name of player' + playerNumber + '....' + playerName); }
-    return {playerName,playerNumber, assignedXO};
-};
-
-// refaire les condtions en fonction de celui qui commence, à determiner 
-let displayControllerModule = (e)=> {
-    if (e.target.innerHTML !== ' ') {
-        window.alert('Cette case a déjà été joué');
-        } else if (countValuesInGameboard.assignedXO == null && firstPlayerFunc.firstPlayer === 0) {
-            e.target.innerHTML = "X";
-        } else if (countValuesInGameboard.assignedXO == null && firstPlayerFunc.firstPlayer === 1) {
-            e.target.innerHTML = "O";
-        }else if ((firstPlayerFunc.firstPlayer === 0 && countValuesInGameboard % 2 !== 0) || (firstPlayerFunc.firstPlayer === 1 && countValuesInGameboard % 2 == 0))  { 
-        e.target.innerHTML = "O";
-    }   else if ((firstPlayerFunc.firstPlayer === 0 && countValuesInGameboard % 2 == 0) || (firstPlayerFunc.firstPlayer === 1 && countValuesInGameboard % 2 !== 0)) {
-        e.target.innerHTML = "X";
-    }
+function handleClick(e) {
+  const cell = e.target
+  const currentClass = circleTurn ? CIRCLE_CLASS : X_CLASS
+  placeMark(cell, currentClass)
+  if (checkWin(currentClass)) {
+    endGame(false)
+  } else if (isDraw()) {
+    endGame(true)
+  } else {
+    swapTurns()
+    setBoardHoverClass()
+  }
 }
 
-let countValuesInGameboard = (() => {
-    let boxesArr = [];
-    let divs = document.querySelectorAll('.grid-box').forEach(function(e){
-        boxesArr.push(e.innerHTML);
-    });
-    const boxesStr = boxesArr.join('');
-    let assignedXO = boxesStr.match(/[A-Z]/g);
-    if (assignedXO === null) {
-        return 0;
-      }
-      console.log(assignedXO.length)
-   return assignedXO.length
-})()
+function endGame(draw) {
+  if (draw) {
+    winningMessageTextElement.innerText = 'Egalité!'
+  } else {
+    winningMessageTextElement.innerText = `${circleTurn ? "O" : "X"} l'emporte !`
+  }
+  winningMessageElement.classList.add('show')
+}
 
-let renderArrayToGameBoardfunction = (() => {
-    const divBoxes = document.querySelectorAll('.grid-box');
-    const boxesArr = Array.from(divBoxes);
+function isDraw() {
+  return [...cellElements].every(cell => {
+    return cell.classList.contains(X_CLASS) || cell.classList.contains(CIRCLE_CLASS)
+  })
+}
 
-    for (i=0;i < boxesArr.length;i++) {
-      gameboardModule.gameboard.splice(i, 1, boxesArr[i]);
-    }
-    console.log(gameboardModule.gameboard)
-    return gameboardModule.gameboard;
-})
+function placeMark(cell, currentClass) {
+  cell.classList.add(currentClass)
+}
 
-let addMarkToGameBoardArray = (() => {
-    const divsBoxes = document.querySelectorAll('.grid-box');
-    Array.from(divsBoxes).forEach(el => {
-    el.addEventListener('click', displayControllerModule)
+function swapTurns() {
+  circleTurn = !circleTurn
+}
+
+function setBoardHoverClass() {
+  board.classList.remove(X_CLASS)
+  board.classList.remove(CIRCLE_CLASS)
+  if (circleTurn) {
+    board.classList.add(CIRCLE_CLASS)
+  } else {
+    board.classList.add(X_CLASS)
+  }
+}
+
+function checkWin(currentClass) {
+  return WINNING_COMBINATIONS.some(combination => {
+    return combination.every(index => {
+      return cellElements[index].classList.contains(currentClass)
     })
-})()
- 
-let theWinnerIs = (() => {
-    let gameboardStr = gameboardModule.gameboard.join('');
-    for (i=0;i<gameboardStr.length;i++){
-        if (/XXX/.test(gameboardStr) || /OOO/.test(gameboardStr)) { // horizontal winning case
-            return windows.alert('Bravo, finito pipo ! c\'est gagné!');
-        } else if (/X\D{2}X\D{2}X/.test(gameboardStr) || /O\D{2}O\D{2}O/.test(gameboardStr)) {  // vertical winning case
-            return windows.alert('Bravo, finito pipo ! c\'est gagné!');
-        } else if (/^X\D{3}X\D{3}X$/.test(gameboardStr) || /^O\D{3}O\D{3}O$/.test(gameboardStr)) { // first diagonal winning case
-            return windows.alert('Bravo, finito pipo ! c\'est gagné!');
-        } else if (/^D{2}X\DX\DX\D{3}$/.test(gameboardStr) || /^O\D{3}O\D{3}O$/.test(gameboardStr)) { // second diagonal winning case 
-            return windows.alert('Bravo, finito pipo ! c\'est gagné!');
-        }
-    }
-})()
-
+  })
+}
